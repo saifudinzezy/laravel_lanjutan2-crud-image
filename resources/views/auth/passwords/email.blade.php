487371182913bb4b603992a57db0d0e1fc4cbc7a
saifudin <saifudin.zezy7@gmail.com>
2019-04-21 08:20:57 +0700

    mash error lupa password

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -15,7 +15,8 @@
                     @endif
 
                     <form class="form-horizontal" method="POST" action="{{ route('password.email') }}">
-                        {{ csrf_field() }}
+                        {{-- {{ csrf_field() }} --}}
+                        {!! csrf_field() !!}
 
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
                             <label for="email" class="col-md-4 control-label">E-Mail Address</label>
